54def6f024de6a9a8f9e4a3ebfafff391e897325
Takafumi Arakaki <aka.tkf@gmail.com>
2019-03-31 12:24:19 -0700

    Add install_cli

--- src/JuliaManager.jl
+++ src/JuliaManager.jl
@@ -3,4 +3,38 @@ module JuliaManager
 include("SysImageHack/SysImageHack.jl")
 using .SysImageHack: compile_patched_sysimage
 
+bundled_jlm() = joinpath(dirname(@__DIR__), "jlm", "jlm")
+
+"""
+    JuliaManager.install_cli([destdir = "~/.julia/bin"])
+
+Install `jlm` CLI at `destdir`.
+"""
+function install_cli(destdir=joinpath(homedir(), ".julia", "bin"))
+    destpath = joinpath(destdir, "jlm")
+    if isfile(destpath)
+        @info "CLI `jlm` is already installed at $destpath"
+    else
+        @info "Installing CLI `jlm` at $destpath"
+        mkpath(destdir)
+        symlink(bundled_jlm(), destpath)
+    end
+
+    installed = Sys.which("jlm")
+    if installed === nothing
+        @warn "`jlm` is not on PATH.  Please make sure $destdir is in PATH."
+    elseif realpath(installed) != realpath(destpath)
+        @warn """
+        A `jlm` program different from the one just installed may be
+        executed.  Please check your PATH.
+        Program to be executed:
+            $installed
+        Program that is installed:
+            $destpath
+        """
+    end
+
+    return
+end
+
 end # module
