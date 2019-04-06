bf7e77e07c6e919b8e0b44f94ec027433aba49c8
Takafumi Arakaki <aka.tkf@gmail.com>
2019-04-05 21:39:22 -0700

    Bump to 0.1.1-DEV

--- jlm/docs/source/conf.py
+++ jlm/docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = "2019, Takafumi Arakaki"
 author = "Takafumi Arakaki"
 
 # The short X.Y version
-version = "0.1.0"
+version = "0.1.1"
 # The full version, including alpha/beta/rc tags
-release = "0.1.0"
+release = "0.1.1-DEV"
 
 
 # -- General configuration ---------------------------------------------------
